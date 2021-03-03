e9720288c25bfd4828452cf6af30dec8cbfd4c26
alebusi <allebusi@gmail.com>
2021-03-03 16:02:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,14 @@ function mysel(div) {
 }
 
 function scriviTxt(car) {
-  testo=document.getElementsById("testo")+car;
-  document.getElementsById("testo").innerHTML = testo;
+  if (car == '_' ) {
+    car=" ";
+  }
+  if (car == "x") {
+    testo=testo.substring(0, testo.length - 1);
+  }
+  else {
+    testo=document.getElementById("testo").innerHTML+car;
+  }
+  document.getElementById("testo").innerHTML = testo;
 }
