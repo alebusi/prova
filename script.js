dc27d64d8534083b0c99458d3200774847be1e1c
alebusi <allebusi@gmail.com>
2021-03-04 11:03:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function mysel(div) {
-  var suona = new Audio('campo.mp3');
-  var className = div.getAttribute("class");
-  var x = document.getElementsByClassName("giallo");
-  if (x.length > 0) {
-     x[0].className = "bianco";
-  }
-  if (className=="bianco") {
-      div.className = "giallo";
-       }
-  else if (className == "giallo") {
-       div.className = "bianco";
-       }
-  suona.play();
-}
-
 function scriviTxt(car) {
   if (car == '_' ) {
     car=" ";
