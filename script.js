b3e0adae321a8b477ae186e41a59bb99225fe083
alebusi <allebusi@gmail.com>
2021-03-06 17:52:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+var intestazione='<svg viewBox="0 0 100 100"> <path d=\"';
+var coda="\" /></svg>";
+var testo="";
+
 function scriviTxt(car) {
-  if (car == '_' ) {
-    car=" ";
-  }
   if (car == "x") {
     testo=testo.substring(0, testo.length - 1);
   }
+  else if (car == '<') {
+    testo=testo+intestazione;
+  }
+  else if (car == ">") {
+    testo=testo+coda;
+  }
   else {
-    testo=document.getElementById("testo").innerHTML+car;
+    testo=testo+car;
   }
   document.getElementById("testo").innerHTML = testo;
 }
